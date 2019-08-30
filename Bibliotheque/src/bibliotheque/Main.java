0156679d4197a034434ea30da2590b0428f8973f
drouart <julien.drouart1@viacesi.fr>
2019-08-30 11:26:04 +0200

    recup

--- Bibliotheque/src/bibliotheque/Main.java
+++ Bibliotheque/src/bibliotheque/Main.java
@@ -17,12 +17,12 @@ public class Main {
 	{
 	 try
         {           
-            Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/documentation", "root","");
+            Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root","");
             Statement st=cn.createStatement();
             ResultSet rs=st.executeQuery("select * from config");
             while(rs.next())
             {
-                System.out.println(rs.getString("Num_com"));
+                System.out.println(rs.getString("value"));
             }
         }
         catch(SQLException e)
