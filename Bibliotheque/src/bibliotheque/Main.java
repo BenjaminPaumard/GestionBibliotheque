ef8104fc32f28743e9821de23296f43433471e8d
Alban Patault <alban.patault@applidev.tv>
2019-08-30 11:25:20 +0200

    alban

--- Bibliotheque/src/bibliotheque/Main.java
+++ Bibliotheque/src/bibliotheque/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		Fenetre.main(args);;
 	    JFrame fenetre = new JFrame();
 	    fenetre.setVisible(true);
 	}
